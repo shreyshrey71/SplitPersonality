db0c13ca23e6cdc31dfa9f5f48b22e9a7751a577
TejusR <rameshtejus@gmail.com>
2019-11-09 19:09:47 +0530

    [feat] Main screen -Add MainScreenActivity -Add value file dimens.xml -Make changes in splash screen

---
+++ app/src/main/java/com/android/splitpersonality/Main_Screen/MainScreenActivity.java
@@ -0,0 +1,36 @@
+package com.android.splitpersonality.Main_Screen;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.android.splitpersonality.R;
+
+public class MainScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
