4fabb936457a9e966a643765407366e336b17cb9
TejusR <rameshtejus@gmail.com>
2019-11-10 19:26:12 +0530

    [feat] Profile list in main screen and add create_profile screen -Add recycler view for the profiles -add create_profie acrivity

---
+++ app/src/main/java/com/android/splitpersonality/Main_Screen/ProfileListAdapter.java
@@ -0,0 +1,43 @@
+package com.android.splitpersonality.Main_Screen;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.splitpersonality.R;
+
+public class ProfileListAdapter extends RecyclerView.Adapter<ProfileListAdapter.ViewHolder> {
+    Context mContext;
+
+    public ProfileListAdapter(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(mContext).inflate(R.layout.profile_list,null);
+        ViewHolder holder=new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 5;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
