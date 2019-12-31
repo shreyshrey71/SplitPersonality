dc93b70163f2344df7a7daf2039d6318016d6809
htg2704 <harshwardhan2704@gmail.com>
2019-12-31 18:57:15 +0530

    wave splash

--- app/src/main/java/com/android/splitpersonality/Splash_Screen/SplashActivity.java
+++ app/src/main/java/com/android/splitpersonality/Splash_Screen/SplashActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.android.splitpersonality.Main_Screen.MainScreenActivity;
 import com.android.splitpersonality.R;
@@ -12,12 +16,25 @@ import com.android.splitpersonality.R;
 public class SplashActivity extends AppCompatActivity {
 
     Handler mHandler;
-
+    Button b1;
+    ImageView split,i1,i2,i3,i4;
+    Animation for_button, for_i1,for_i2,for_i3,for_i4,for_split;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-
+        setContentView(R.layout.activity_splash1);
+        /*b1 = (Button) findViewById(R.id.button3);
+        split = (ImageView) findViewById(R.id.imageView6);
+        i1 = (ImageView) findViewById(R.id.imageView2);
+        i2 = (ImageView) findViewById(R.id.imageView3);
+        i3 = (ImageView) findViewById(R.id.imageView4);
+        i4 = (ImageView) findViewById(R.id.imageView5);
+        for_button = AnimationUtils.loadAnimation(this,R.anim.for_button);
+        b1.setAnimation(for_button);
+        for_split = AnimationUtils.loadAnimation(this,R.anim.for_split);
+        for_i1 = AnimationUtils.loadAnimation(this,R.anim.for_images);
+        split.setAnimation(for_split);
+        i1.setAnimation(for_i1);*/
         mHandler=new Handler();
         mHandler.postDelayed(new Runnable() {
             @Override
@@ -25,7 +42,7 @@ public class SplashActivity extends AppCompatActivity {
                 Intent intent=new Intent(getApplicationContext(), MainScreenActivity.class);
                 startActivity(intent);
             }
-        },500);
+        },2000);
     }
 
     @Override
