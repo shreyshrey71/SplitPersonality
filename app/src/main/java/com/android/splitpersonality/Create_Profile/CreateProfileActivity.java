035ef9ef0220dada9c64d945845c93f2ad5da9ac
htg2704 <harshwardhan2704@gmail.com>
2020-01-11 17:44:31 +0530

    commit

--- app/src/main/java/com/android/splitpersonality/Create_Profile/CreateProfileActivity.java
+++ app/src/main/java/com/android/splitpersonality/Create_Profile/CreateProfileActivity.java
@@ -3,14 +3,27 @@ package com.android.splitpersonality.Create_Profile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Switch;
 
 import com.android.splitpersonality.R;
 
 public class CreateProfileActivity extends AppCompatActivity {
 
+    Switch bluetooth, airplane;
+    boolean b,a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_profile);
+
+        bluetooth = findViewById(R.id.Bluetooth);
+        airplane = findViewById(R.id.airplane_mode);
+
+        if(bluetooth.isChecked())
+            b = true;
+        if(airplane.isChecked())
+            a = true;
+
+
     }
 }
--- app/src/main/java/com/android/splitpersonality/Splash_Screen/SplashActivity.java
+++ app/src/main/java/com/android/splitpersonality/Splash_Screen/SplashActivity.java
@@ -52,6 +52,7 @@ public class SplashActivity extends AppCompatActivity {
     }
 
     @Override
+
     public void onBackPressed() {
         super.onBackPressed();
         finish();
