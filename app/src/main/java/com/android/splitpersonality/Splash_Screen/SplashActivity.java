369893b12fe0430bbf48427a0dae837113b73f9a
htg2704 <harshwardhan2704@gmail.com>
2020-01-15 16:55:11 +0530

    Common Intent UI

--- app/src/main/java/com/android/splitpersonality/Splash_Screen/SplashActivity.java
+++ app/src/main/java/com/android/splitpersonality/Splash_Screen/SplashActivity.java
@@ -42,7 +42,7 @@ public class SplashActivity extends AppCompatActivity {
                 Intent intent=new Intent(getApplicationContext(), MainScreenActivity.class);
                 startActivity(intent);
             }
-        },2000);
+        },1000);
     }
 
     @Override
