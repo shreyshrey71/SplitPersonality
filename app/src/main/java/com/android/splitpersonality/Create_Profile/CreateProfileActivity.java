af02f4e3581d9ec774dfb21aa182039bf9265fbf
Anjalidubey01 <kumarianjalidubey012000@gmail.com>
2020-01-24 20:17:20 +0530

    Added floating button to make dummy profile and also implemented triggering of that profile with WIFI

--- app/src/main/java/com/android/splitpersonality/Create_Profile/CreateProfileActivity.java
+++ app/src/main/java/com/android/splitpersonality/Create_Profile/CreateProfileActivity.java
@@ -2,19 +2,58 @@ package com.android.splitpersonality.Create_Profile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.android.splitpersonality.R;
 import com.android.splitpersonality.SplitTransition;
 
-public class CreateProfileActivity extends AppCompatActivity {
+import java.util.Calendar;
 
+public class CreateProfileActivity extends AppCompatActivity {
+    TimePicker timePicker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SplitTransition.prepareAnimation(this);
+        SplitTransition.prepareAnimation(CreateProfileActivity.this);
         setContentView(R.layout.activity_create_profile);
+      //
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+        findViewById(R.id.buttonAlarm).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //We need a calendar object to get the specified time in millis
+                //as the alarm manager method takes time in millis to setup the alarm
+                Calendar calendar = Calendar.getInstance();
+                if (android.os.Build.VERSION.SDK_INT >= 23) {
+                    calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH),
+                            timePicker.getHour(), timePicker.getMinute(), 0);
+                } else {
+                    calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH),
+                            timePicker.getCurrentHour(), timePicker.getCurrentMinute(), 0);
+                }
 
-        SplitTransition.animate(this, 500);
+
+                startAlert(calendar.getTimeInMillis());
+            }
+        });
+   SplitTransition.animate(this, 500);
+
+    }
+
+    public void startAlert(long time){
+        Intent intent = new Intent(this, MyBroadcastReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                this.getApplicationContext(), 234324243, intent, 0);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        //alarm to repeat everyday
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,time,24*60*1000*60, pendingIntent);
+        Toast.makeText(this, "Alarm set", Toast.LENGTH_LONG).show();
     }
 }
---
+++ app/src/main/java/com/android/splitpersonality/Create_Profile/MyBroadcastReceiver.java
@@ -0,0 +1,42 @@
+package com.android.splitpersonality.Create_Profile;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String str="helo";
+        //if the connected WIFI is "HP" then trigger the alarm
+    if(getWifiName(context).equals("\"HP\"")){
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alarmUri == null) {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
+        ringtone.play();
+        Log.d("Alarm", "Alarm just fired"+getWifiName(context)+str);}
+    }
+    //method to get the wifi
+    public String getWifiName(Context context) {
+        WifiManager manager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        if (manager.isWifiEnabled()) {
+            WifiInfo wifiInfo = manager.getConnectionInfo();
+            if (wifiInfo != null) {
+                NetworkInfo.DetailedState state = WifiInfo.getDetailedStateOf(wifiInfo.getSupplicantState());
+                if (state == NetworkInfo.DetailedState.CONNECTED || state == NetworkInfo.DetailedState.OBTAINING_IPADDR) {
+                    return wifiInfo.getSSID();
+                }
+            }
+        }
+        return null;
+    }
+}
